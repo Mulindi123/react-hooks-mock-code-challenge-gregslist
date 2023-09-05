05ba022153d430a1bc6131cd33f9a8a0f1304f5d
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-09-05 08:26:17 +0300

    feat:renders the initial fetch

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+const [listings, setListings] =useState([])
+
+useEffect(()=>{
+  fetch("http://localhost:6001/listings")
+  .then(r=>r.json())
+  .then(data=>setListings(data))
+}, [])
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map((listing)=>{
+          return(
+            <ListingCard listing={listing}/>
+          )
+        })}
       </ul>
     </main>
   );
